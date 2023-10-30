32f4c34424c1be534c520dc74f593eb94814dc42
mklol00 <dlwodn97@naver.com>
2023-10-30 11:36:14 +0900

    더하기 구현

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,9 +1,16 @@
 public class Calc {
     public static int run(String exp) {
-        if(exp.contains("-")){
+        if (exp.isBlank()) {
             return 0;
         }
-        else
-            return 20;
+        if (exp.contains("-")) {
+            return 0;
+        }
+
+        String[] exps = exp.split(" \\+ ");
+        int num1 = Integer.parseInt(exps[0]);
+        int num2 = Integer.parseInt(exps[1]);
+        return num1 + num2;
+
     }
 }
